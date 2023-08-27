767709f461f14685c49290437dcda3ccc9d3322b
Pablo Luan <pablo-544@hotmail.com>
2023-08-27 12:33:01 -0300

    separação dos projetos por pastas

--- web/src/components/login-page/LoginFormField.tsx
+++ web/src/components/login-page/LoginFormField.tsx
@@ -13,7 +13,7 @@ export default function LoginOrRegisterField() {
     }
 
     return (
-        <div className={`shadow-customLogin bg-white rounded-[.625rem] relative overflow-hidden w-[48rem] max-w-full min-h-[30rem] ${isSignUp ? 'right-panel-active' : ''}`} id="toggleButton">
+        <div className={`shadow-customLogin bg-white rounded-[.625rem] relative overflow-hidden w-[48rem] max-w-full min-h-[30rem]`} id="toggleButton">
 
             {isSignUp ? (
                 <div className={`left-0 w-1/2 z-30 translate-x-full absolute top-0 h-full transition-all duration-[600ms] ease-in-out `}>
@@ -31,20 +31,20 @@ export default function LoginOrRegisterField() {
                 </div>
             ) : (
                 <div className={`left-0 w-1/2 z-20 absolute top-0 h-full transition-all duration-[600ms] ease-in-out `}>
-                <form action="#" className='flex bg-white items-center justify-center flex-col px-[3.125rem] text-center h-full' >
-                    <h2 className='text-[2rem] font-bold'>Sign In</h2>
-                    
-                    <SocialLogin />
+                    <form action="#" className='flex bg-white items-center justify-center flex-col px-[3.125rem] text-center h-full' >
+                        <h2 className='text-[2rem] font-bold'>Sign In</h2>
+                        
+                        <SocialLogin />
 
-                    <span className='text-xs'>or use your account</span>
-                    <input type="email" placeholder="Email" className='bg-[#eee] border-none px-4 py-3 m-2 w-full' />
-                    <input type="password" placeholder="Password" className='bg-[#eee] border-none px-[.938rem] py-3 m-2 w-full' />
-                    <Link href="#" className='my-4'>Forgot your password?</Link>
-                    <button className='rounded-[1.25rem] border border-solid border-black bg-black text-white font-bold uppercase py-3 px-11 tracking-[1px] text-xs'>Sign In</button>
-                </form>
-            </div>
-
-            ) }
+                        <span className='text-xs'>or use your account</span>
+                        <input type="email" placeholder="Email" className='bg-[#eee] border-none px-4 py-3 m-2 w-full' />
+                        <input type="password" placeholder="Password" className='bg-[#eee] border-none px-[.938rem] py-3 m-2 w-full' />
+                        <Link href="#" className='my-4'>Forgot your password?</Link>
+                        <button className='rounded-[1.25rem] border border-solid border-black bg-black text-white font-bold uppercase py-3 px-11 tracking-[1px] text-xs'>Sign In</button>
+                    </form>
+                </div>
+                ) 
+            }
 
 
             <div className={`absolute top-0 left-1/2 w-1/2 h-full overflow-hidden transition-transform ease-in-out duration-[600ms] z-40 ${isSignUp ? '-translate-x-full' : ''}`}>
